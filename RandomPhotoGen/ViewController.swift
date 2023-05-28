98e4e4c95383c9b32a51e2770d6c9af373aee223
Andrew Fowose <usafowose@gmail.com>
2023-05-28 02:48:06 -0400

    Initial Commit

---
+++ RandomPhotoGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoGen
+//
+//  Created by Andrew Fowose on 5/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
